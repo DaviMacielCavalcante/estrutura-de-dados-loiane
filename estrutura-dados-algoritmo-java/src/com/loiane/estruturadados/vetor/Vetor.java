eda37a65b7fe94eff4bee952ce5509f0eb48981a
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2022-07-24 19:26:03 -0300

    Aula 4 e 5

--- estrutura-dados-algoritmo-java/src/com/loiane/estruturadados/vetor/Vetor.java
+++ estrutura-dados-algoritmo-java/src/com/loiane/estruturadados/vetor/Vetor.java
@@ -38,4 +38,36 @@ public class Vetor {
 		return false;
 	}
 	
+	public String busca(int posicao) {
+		if (!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("Posição inválida!");
+		}
+		return this.elementos[posicao];
+	}
+		
+	public int tamanho() {
+		return this.tamanho;
+	}
+
+	@Override
+	public String toString() {
+		
+		StringBuilder s = new StringBuilder();
+		s.append("[");		
+		
+		for (int i=0; i<this.tamanho-1; i++) {
+			s.append(this.elementos[i]);
+			s.append(", ");
+		}
+		
+		if (this.tamanho>0) {
+			s.append(this.elementos[this.tamanho-1]);
+		}
+		
+		s.append("]");
+		
+		return s.toString();
+	}
+	
+	
 }
